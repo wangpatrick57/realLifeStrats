fcfbd652e6426a854275929c9c93fbb3d248d3a9
Wingspear <ethan.soo@hotmail.com>
2018-04-21 20:43:48 -0700

    Update ViewController.swift

--- RLS/RLS/ViewController.swift
+++ RLS/RLS/ViewController.swift
@@ -16,21 +16,27 @@ class ViewController: UIViewController,CLLocationManagerDelegate {
     let manager=CLLocationManager()
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let location=locations[0]
-        let span:MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
-        let myLocation:CLLocationCoordinate2D=CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
-        let region:MKCoordinateRegion=MKCoordinateRegionMake(myLocation, span)
-        map.setRegion(region, animated: true)
-        self.map.showsUserLocation=true
+        //let location=locations[0]
+         let regionRadius: CLLocationDistance = 1000
+        //let span:MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
+        
+        let myLocation:CLLocationCoordinate2D=CLLocationCoordinate2DMake(122.0322, 37.3230)
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(myLocation, regionRadius, regionRadius)
+        //let region:MKCoordinateRegion=MKCoordinateRegionMake(myLocation, span)
+        map.setRegion(coordinateRegion, animated: true)
+        //self.map.showsUserLocation=true
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         manager.delegate=self
+        locationManager(<#T##manager: CLLocationManager##CLLocationManager#>, didUpdateLocations: <#T##[CLLocation]#>)
         manager.desiredAccuracy=kCLLocationAccuracyBest
         manager.requestAlwaysAuthorization()
         manager.startUpdatingLocation()
+        
+       
         
     }
     
@@ -38,6 +44,8 @@ class ViewController: UIViewController,CLLocationManagerDelegate {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+
     
     
 }
